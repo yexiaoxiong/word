e64e1520e55f85c76b0295d63e1713e843e2f1e9
yexiaoxiong <1678326574@qq.com>
2017-06-26 16:50:46 +0800

    first commit

---
+++ src/We.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Hexin\Project;
+
+use App\Http\Controllers\Controller;
+
+class We extends Controller
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public static function index()
+    {
+       return 'hello word';
+    }
+
+    
+}
